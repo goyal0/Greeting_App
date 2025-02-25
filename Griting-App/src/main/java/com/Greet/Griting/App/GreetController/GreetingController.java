8eab8dae27fb2944666f70ae423207e3d4d4e2f3
piyush goyal <piygoyal30@gmail.com>
2025-02-25 11:35:02 +0530

    [Piyush Goyal] Adding File UC3_GreetinWithName

--- Griting-App/src/main/java/com/Greet/Griting/App/GreetController/GreetingController.java
+++ Griting-App/src/main/java/com/Greet/Griting/App/GreetController/GreetingController.java
@@ -1,5 +1,8 @@
 package com.Greet.Griting.App.GreetController;
 
+import com.Greet.Griting.App.Model.Greeting;
+import com.Greet.Griting.App.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,4 +29,21 @@ public class GreetingController {
         return "{\"message\": \"Greeting deleted!\"}";
     }
 
-}
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/service")
+    public String greetingService() {
+        return greetingService.greeting();
+    }
+
+    private GreetingService GreetingService;
+
+    @GetMapping
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return "{\"message\": \"" + greetingService.getGreetingMessage(firstName, lastName) + "\"}";
+    }
+
+
+    }

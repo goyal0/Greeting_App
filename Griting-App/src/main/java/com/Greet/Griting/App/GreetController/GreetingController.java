f162008fd91bab163f81acf1bfb057008e91b98d
piyush goyal <piygoyal30@gmail.com>
2025-02-25 12:12:47 +0530

    [Piyush Goyal] Adding File UC4_SaveGreeting

--- Griting-App/src/main/java/com/Greet/Griting/App/GreetController/GreetingController.java
+++ Griting-App/src/main/java/com/Greet/Griting/App/GreetController/GreetingController.java
@@ -1,6 +1,9 @@
 package com.Greet.Griting.App.GreetController;
 
+import com.Greet.Griting.App.Model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.Greet.Griting.App.Service.GreetingService;
 
 @RestController
 @RequestMapping("/greeting")
@@ -26,4 +29,25 @@ public class GreetingController {
         return "{\"message\": \"Greeting deleted!\"}";
     }
 
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/service")
+    public String greetingService() {
+        return greetingService.greeting();
+    }
+
+    @Autowired
+    private GreetingService GreetingService;
+
+    @GetMapping
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return "{\"message\": \"" + greetingService.getGreetingMessage(firstName, lastName)+"\"}";
+    }
+
+    @PostMapping("/save")
+    public Greeting saveGreeting(@RequestBody String message) {
+        return greetingService.saveGreeting(message);
+    }
 }
